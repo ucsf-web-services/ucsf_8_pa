26777e205edf6ce3cfecdfba2aa9822436e0796e
Eric Guerin <eric.guerin@ucsf.edu>
2018-12-06 17:23:46 -0800

    Clean up old code references.

--- docroot/modules/custom/ucsf_google_search/src/Plugin/Block/ucsf_google_search_block.php
+++ docroot/modules/custom/ucsf_google_search/src/Plugin/Block/ucsf_google_search_block.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\ucsf_google_search\Plugin\Block;
+
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Form\FormStateInterface;
@@ -51,44 +52,6 @@ MARKUP;
     return AccessResult::allowedIfHasPermission($account, 'access content');
   }
 
-  public function oldCode() {
-      /**
-       * Google Custom Search page
-       */
-      $code = '006142056215064927360:qqvc-py3r80'; //live code
-      $refinement='';
-      if(isset($_GET['search'])) {
-        $search = preg_replace("/\r\n|\r|\n/", ' ', $_GET['search']);
-        $search = filter_xss(htmlspecialchars($search, ENT_QUOTES));
-      }
-
-      $js = <<<EOT
-        (function() {
-          var cx = '{$code}';
-          var gcse = document.createElement('script');
-          gcse.type = 'text/javascript';
-          gcse.async = true;
-          gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(gcse, s);
-          })();
-EOT;
-        //drupal_add_js($js, 'inline');
-        $content =<<<EOL
-        <div class="form-search-block">
-          <form action="{$base_path}search/site" method="get" id="form-search-block">
-              <div class="search-box-container">
-                <gcse:searchbox></gcse:searchbox>
-              </div>
-          </form>
-          <div id="cse">
-            <gcse:searchresults-only queryParameterName="search" {$refinement}></gcse:searchresults-only>
-          </div>
-        </div>
-EOL;
-
-      return $content;
-    }
 
 }
 
