46add7a45c23da220f92c3c1dfeea1992d75dcf8
Austen <austen.tong@ucsf.edu>
2019-01-22 18:14:44 -0800

    integrating d7 pullquote

---
+++ docroot/modules/custom/ckeditor_test/src/Plugin/CKEditorPlugin/ucsfquote.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ckeditor_test\Plugin\CKEditorPlugin\ucsfquote.
+ */
+
+namespace Drupal\ckeditor_test\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "ucsfquote" plugin.
+
+ *
+ * @CKEditorPlugin(
+ *   id = "ucsfquote",
+ *   label = @Translation("ucsfquote"),
+ *   module = "ckeditor_test"
+ * )
+ */
+class ucsfquote extends CKEditorPluginBase {
+
+  /**
+   * Implements \Drupal\ckeditor\Plugin\CKEditorPluginInterface::getDependencies().
+   */
+  public function getDependencies(Editor $editor) {
+    return array();
+  }
+
+  /**
+   * Implements \Drupal\ckeditor\Plugin\CKEditorPluginInterface::getLibraries().
+   */
+  public function getLibraries(Editor $editor) {
+    return array();
+  }
+
+  /**
+   * Implements \Drupal\ckeditor\Plugin\CKEditorPluginInterface::isInternal().
+   */
+  public function isInternal() {
+    return FALSE;
+  }
+
+  /**
+   * Implements \Drupal\ckeditor\Plugin\CKEditorPluginInterface::getFile().
+   */
+  public function getFile() {
+    return drupal_get_path('module', 'ckeditor_test') . '/js/plugins/ucsfquote/plugin.js';
+  }
+
+  /**
+   * Implements \Drupal\ckeditor\Plugin\CKEditorPluginInterface::getConfig().
+   */
+  public function getConfig(Editor $editor) {
+    return array();
+  }
+
+  /**
+   * Implements \Drupal\ckeditor\Plugin\CKEditorPluginButtonsInterface::getButtons().
+   */
+  public function getButtons() {
+    return [
+      'ucsfquote' => [
+        'label' => t('ucsfquote'),
+        'image' => drupal_get_path('module', 'ckeditor_test') . '/js/plugins/ucsfquote/icons/ucsfquote.png',
+      ]
+    ];
+  }
+   /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state, Editor $editor) {
+    return $form;
+  }
+
+
+}
