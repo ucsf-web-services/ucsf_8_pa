8e22aa3e41bf316ff85a64a405595b3de5ec0baa
Brian Fisher <tbfisher@gmail.com>
2019-03-12 11:25:43 -0600

    change author component to byline, use field_date_and_time for date, use field_author and  field_custom_author for author names

--- docroot/modules/custom/ucsf_applenews/src/Normalizer/UcsfApplenewsTextComponentNormalizer.php
+++ docroot/modules/custom/ucsf_applenews/src/Normalizer/UcsfApplenewsTextComponentNormalizer.php
@@ -9,6 +9,7 @@ use ChapterThree\AppleNewsAPI\Document\Margin;
 use ChapterThree\AppleNewsAPI\Document\Styles\ComponentTextStyle;
 use ChapterThree\AppleNewsAPI\Document\Styles\TextStyle;
 use Drupal\applenews\Normalizer\ApplenewsTextComponentNormalizer;
+use Drupal\Core\Field\EntityReferenceFieldItemList;
 use Drupal\Core\TypedData\Exception\MissingDataException;
 use Drupal\Core\Url;
 use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
@@ -23,11 +24,11 @@ class UcsfApplenewsTextComponentNormalizer extends ApplenewsTextComponentNormali
    */
   public function normalize($data, $format = NULL, array $context = []) {
 
-    if ($data['id'] == 'default_text:author' &&
+    if ($data['id'] == 'default_text:byline' &&
       isset($data['component_data']['text']['field_name']) &&
       $data['component_data']['text']['field_name'] == 'field_author'
     ) {
-      return $this->normalizeNewsAuthor($data, $format, $context);
+      return $this->normalizeByline($data, $format, $context);
     }
 
     if ($data['id'] == 'default_text:body' &&
@@ -115,7 +116,7 @@ class UcsfApplenewsTextComponentNormalizer extends ApplenewsTextComponentNormali
   /**
    * News author.
    */
-  protected function normalizeNewsAuthor($data, $format = NULL, array $context = []) {
+  protected function normalizeByline($data, $format = NULL, array $context = []) {
 
     /** @var \Drupal\node\Entity\Node $entity */
     $entity = $context['entity'];
@@ -123,7 +124,7 @@ class UcsfApplenewsTextComponentNormalizer extends ApplenewsTextComponentNormali
     // Generate content.
     /** @var \Drupal\datetime\Plugin\Field\FieldType\DateTimeItem $date */
     try {
-      $date = $entity->get('field_date')->get(0);
+      $date = $entity->get('field_date_and_time')->get(0);
     }
     catch (MissingDataException $e) {
       return FALSE;
@@ -143,12 +144,18 @@ class UcsfApplenewsTextComponentNormalizer extends ApplenewsTextComponentNormali
         $author->get('field_last_name')->getString(),
       ]);
     }
-    if (!$names) {
-      return FALSE;
-    }
     $names = array_map(function ($name) {
       return implode(' ', $name);
     }, $names);
+    /** @var \Drupal\Core\Field\EntityReferenceFieldItemList $authors */
+    $authors = $entity->get('field_custom_author');
+    /** @var \Drupal\taxonomy\Entity\Term $author */
+    foreach ($authors->referencedEntities() as $author) {
+      $names[] = $author->getName();
+    }
+    if (!$names) {
+      return FALSE;
+    }
     $component = new Author("By " . implode(', ', $names) .
       ' on ' . $date . '.');
 
