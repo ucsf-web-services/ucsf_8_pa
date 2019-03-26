32015c4c211b461d5efc1cbb8a996f6d1f537b28
Eric Guerin <eric.guerin@ucsf.edu>
2019-03-26 02:29:01 -0700

    Try to add search menu item, fix some issues with the Twitter layout, etc.

--- docroot/modules/contrib/twitter_tweets/src/Plugin/Block/TweetsBlock.php
+++ docroot/modules/contrib/twitter_tweets/src/Plugin/Block/TweetsBlock.php
@@ -34,9 +34,34 @@ class TweetsBlock extends BlockBase {  /**
     $tweets = $twitter->setGetfield($getfield) ->buildOauth($url, $requestMethod)->performRequest();
 
     $tweets = json_decode($tweets);
+    //https://benmarshall.me/parse-twitter-hashtags/
+
+    $reg_Url = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+    //$reg_Url = "/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/";
+    //http://twitter.com/' + username
+    $reg_User = "/[@]+[A-Za-z0-9-_]+/";
+    // http://search.twitter.com/search?q= + tag
+    $reg_Hash = "/[#]+[A-Za-z0-9-_]+/";
+
     foreach($tweets as $tweet) {
-      $tweet->full_text = check_markup($tweet->full_text, 'full_html');
-      $tweet->full_text = preg_replace('/https:\/\/t.co\/.*/' ,'' ,$tweet->full_text);
+      //$tweet->full_text = check_markup($tweet->full_text, 'full_html');
+      //$tweet->full_text = '   ';
+      $tweet->full_text = preg_replace('~[\r\n]~', ' ', $tweet->full_text);
+
+      if(preg_match($reg_Url, $tweet->full_text, $url)) {
+        $tweet->full_text =  preg_replace($reg_Url, "<a href=\"{$url[0]}\">{$url[0]}</a>", $tweet->full_text);
+      }
+
+      if(preg_match($reg_User, $tweet->full_text, $url)) {
+        $tweet->full_text =  preg_replace($reg_User, "<a href=\"http://twitter.com/{$url[0]}\">{$url[0]}</a>", $tweet->full_text);
+      }
+
+      if(preg_match($reg_Hash, $tweet->full_text, $url)) {
+        $hash = str_ireplace('#','', $url[0]);
+        $tweet->full_text =  preg_replace($reg_Hash, "<a href=\"https://twitter.com/hashtag/{$hash}?src=hash\">{$url[0]}</a>", $tweet->full_text);
+      }
+
+      //$tweet->full_text = preg_replace('/https:\/\/t.co\/.*/' ,'' ,$tweet->full_text);
       $cleanTweets[] = $tweet;
     }
 
