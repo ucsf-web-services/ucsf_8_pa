94ec16de5958056500b21f9c43f67c697c3b7566
Eric Guerin <eric.guerin@ucsf.edu>
2019-03-30 19:53:50 -0700

    Fix the Slick Carousel for Youtube, so it works responsive, so Jayson has something to go on there.  Also fixed the overflow issue on the topics carousel which was making all the mobile views screwed up.

--- docroot/themes/custom/ucsf/assets/js/ucsf_slick_youtube.js
+++ docroot/themes/custom/ucsf/assets/js/ucsf_slick_youtube.js
@@ -7,7 +7,33 @@
 
       $('.youtube-list__videos').not('.slick-initialized').slick({
         slidesToShow: 4,
-        infinite: false
+        slidesToScroll: 1,
+        infinite: false,
+        responsive: [{
+          breakpoint: 1050,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        }, {
+          breakpoint: 850,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        }, {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }, {
+          breakpoint: 320,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
       });
     }
   };
--- docroot/themes/custom/ucsf/js/ucsf_slick_topics.js
+++ docroot/themes/custom/ucsf/js/ucsf_slick_topics.js
@@ -1,13 +1,42 @@
 /* eslint-disable */
 (function ($) {
+
   Drupal.behaviors.slickTopics = {
     attach: function (context) {
 
       $('.topics-list__topics').not('.slick-initialized').slick({
-        slidesToShow: 4
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        mobileFirst: true,
+        infinite: false,
+        dots: false
+        /*
+        responsive: [
+          {
+            breakpoint: 999,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4
+            }
+          },
+          {
+            breakpoint: 849,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+        */
       });
-
     }
   };
-  
+
 })(jQuery);
