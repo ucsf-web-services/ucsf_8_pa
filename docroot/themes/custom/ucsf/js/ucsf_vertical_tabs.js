7b7ea24e8446f56c19b1f6a835757652088b8510
Zakiya <zakiya@chapterthree.com>
2019-04-01 11:54:41 -0700

    UCSFD8-343 - Kill vertical tabs on small screens.

--- docroot/themes/custom/ucsf/js/ucsf_vertical_tabs.js
+++ docroot/themes/custom/ucsf/js/ucsf_vertical_tabs.js
@@ -4,7 +4,30 @@
   Drupal.behaviors.verticalTabs = {
     attach: function (context, settings) {
       
-      $( ".js-tabs" ).tabs().addClass("ui-helper-clearfix");
+      
+      var mediaQuery = window.matchMedia('(min-width: 850px)');
+      
+      mediaQuery.addListener(tabAccordionSwitch);
+      
+      function tabAccordionSwitch(mediaQuery) {
+        
+        // If large screen.
+        if (mediaQuery.matches) {
+          
+          // Create tabs.
+          $(".js-tabs").tabs().addClass("ui-helper-clearfix");
+  
+        // If small screen.
+        } else {
+  
+          // Destroy tabs if they exist.
+          if ($('.ui-tabs').length > 0) {
+            $(".js-tabs").tabs('destroy')
+          }
+        }
+      }
+      
+      tabAccordionSwitch(mediaQuery);
       
     }
   };
