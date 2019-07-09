61cb78e53643830cdf9189baf60b66baafe59464
Jayson Jaynes <jason.jaynes@ucsf.edu>
2019-07-09 15:09:32 -0700

    Add in unpublished state

--- docroot/modules/custom/ucsf_locations_map/page--node--map.tpl.php
+++ docroot/modules/custom/ucsf_locations_map/page--node--map.tpl.php
@@ -3,8 +3,8 @@
   <div id="map"></div>
 </div>
 <div id="locations-list">
-  <h2 class="title"><?php print $title; ?></h2>
-  <div class="loading"><h3>Loading...</h3></div>
+  <h1 class="title"><?php print $title; ?></h1>
+  <div class="loading"><h2>Loading...</h2></div>
 </div>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAcs-kPsboJFiEYz7o9u_KGDu_JIUUlVEo"></script>
