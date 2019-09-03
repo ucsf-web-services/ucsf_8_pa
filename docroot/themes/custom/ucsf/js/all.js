84993e92b1bb4e970202accea693e64593ad3d22
Sasha Miller <sashazm85@gmail.com>
2019-09-03 16:36:38 -0700

    UCSFD8-655 replaced i to span for icons

--- docroot/themes/custom/ucsf/js/all.js
+++ docroot/themes/custom/ucsf/js/all.js
@@ -18,8 +18,8 @@
                 duplicate: false,
                 prependTo: '.slicknav-placeholder',
                 label: 'Main Menu',
-                openedSymbol: '<i class="fas fa-angle-up" aria-hidden="true">',
-                closedSymbol: '<i class="fas fa-angle-down" aria-hidden="true">'
+                openedSymbol: '<span class="fas fa-angle-up">',
+                closedSymbol: '<span class="fas fa-angle-down">'
             });
 
             //var height = $(window).height();
