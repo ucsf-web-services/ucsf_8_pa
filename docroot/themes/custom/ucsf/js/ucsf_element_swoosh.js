f0e008401d9c8b078f4407dab601eac522443bb1
Sasha Miller <sashazm85@gmail.com>
2019-10-15 16:00:53 -0700

    UCSFD8-864 Animation for elements based on location in viewport

---
+++ docroot/themes/custom/ucsf/js/ucsf_element_swoosh.js
@@ -0,0 +1,23 @@
+/**
+* Animation of elements based on location in the viewport.
+*/
+
+(function () {
+  Drupal.behaviors.elementSwoosh = {
+    attach: function (context, settings) {
+      // element to be animated
+      const elementSwoosh = document.querySelector('.element-swoosh');
+
+      const observer = new IntersectionObserver((entry, observer) => {
+        const targetElement = entry[0].target;
+        if (entry[0].intersectionRatio > 0) {
+          // adds the class responsible for animation on the first viewport appearance
+          targetElement.classList.add('element-swoosh--in-viewport');
+          observer.unobserve(targetElement);
+        }
+      })
+
+      observer.observe(elementSwoosh);
+    }
+  };
+})();
