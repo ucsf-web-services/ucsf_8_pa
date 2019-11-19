7aec767cff44831a1d462be7c9b7fa4e0e854b45
Sasha Miller <sashazm85@gmail.com>
2019-11-19 10:59:48 -0800

    debug

--- hooks/common/post-db-copy/drush-dev-setup.sh
+++ hooks/common/post-db-copy/drush-dev-setup.sh
@@ -12,8 +12,10 @@ target_env=$2
 drush_alias=$site'.'$target_env
 
 # Enable Stage File Proxy so that files don't need to be copied from Prod.
+echo "Enabling Stage File Proxy"
 drush @$drush_alias en stage_file_proxy
 drush @$drush_alias config-set stage_file_proxy.settings origin "https://www.ucsf.edu"
 
 # Clear the cache.
+echo "Clearing Cache"
 drush @$drush_alias cr
