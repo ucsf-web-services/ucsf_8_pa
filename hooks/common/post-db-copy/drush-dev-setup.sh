6d6970e48de81f24118ee9d8598c823f072ee0df
Sasha Miller <sashazm85@gmail.com>
2019-11-19 12:10:28 -0800

    debug

--- hooks/common/post-db-copy/drush-dev-setup.sh
+++ hooks/common/post-db-copy/drush-dev-setup.sh
@@ -14,16 +14,14 @@ drush_alias=$site'.'$target_env
 echo "Site: $site"
 echo "Target Env: $target_env"
 
-test_env="ode5"
-
 # Check if this is an On-Demand Environmnet starting with "ode" prefix.
-if [[ $test_env = ode* ]]; then
+if [ "$target_env" != 'prod' ]; then
   # Enable Stage File Proxy so that files don't need to be copied from Prod.
   echo "Enabling Stage File Proxy"
-  # drush @$drush_alias en stage_file_proxy -y
-  # drush @$drush_alias config-set stage_file_proxy.settings origin "https://www.ucsf.edu" -y
+  drush @$drush_alias en stage_file_proxy -y
+  drush @$drush_alias config-set stage_file_proxy.settings origin "https://www.ucsf.edu" -y
 fi
 
 # Clear the cache.
 echo "Clearing Cache"
-# drush @$drush_alias cr
+drush @$drush_alias cr
