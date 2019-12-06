e48c1744d3a9f91880e870ad7b5a38979a64fd7a
Sasha Miller <sashazm85@gmail.com>
2019-12-05 20:14:19 -0800

    UCSFD8-884 smoother transition for thext rotation. Rotation for 'page not found'

--- docroot/themes/custom/ucsf/assets/js/ucsf_search_placeholder_rotation.js
+++ docroot/themes/custom/ucsf/assets/js/ucsf_search_placeholder_rotation.js
@@ -3,25 +3,35 @@
 
 (function () {
   // amount of time before next transition
-  var delay = 1500;
-  var input = document.querySelector('[data-placeholder]');
+  var delay = 2000;
+  var selectors = ['#search', '[data-placeholder]'];
 
-  // Rets the first placeholder word ('search') to a variable
-  var stationary = input.placeholder.split(' ').shift();
+  selectors.forEach(function (item) {
+    var input = document.querySelector(item);
+    if (input != null) {
+      // Gets the first placeholder word ('search') to a variable
+      var stationary = input.placeholder.split(' ').shift();
 
-  // removes first word from placeholder text
-  var trimmedString = input.placeholder.replace(stationary, '').trim();
+      // removes first word from placeholder text
+      var trimmedString = input.placeholder.replace(stationary, '').trim();
 
-  // Removes commas and white spaces before making an array of placeholder text
-  var placeholderArr = trimmedString.replace(/,\s*/g, ',').split(',');
+      // Removes commas and white spaces before making an array of placeholder text
+      var placeholderArr = trimmedString.replace(/,\s*/g, ',').split(',');
 
-  var count = 0;
-  setInterval(function () {
-    input.setAttribute('placeholder', stationary + ' ' + placeholderArr[count]);
-    count++;
-    if (count >= placeholderArr.length) {
-      count = 0;
+      var count = 0;
+      setInterval(function () {
+        input.classList.add("fade-in");
+        input.setAttribute('placeholder', stationary + ' ' + placeholderArr[count]);
+        // faking the fade in since
+        setTimeout(function () {
+          input.classList.remove("fade-in");
+        }, 300);
+        count++;
+        if (count >= placeholderArr.length) {
+          count = 0;
+        }
+      }, delay);
     }
-  }, delay);
+  });
 })();
 //# sourceMappingURL=ucsf_search_placeholder_rotation.js.map
