1928f011ab7d52d4e9317a3b0a9cb12cf93f1ce7
Sasha Miller <sashazm85@gmail.com>
2019-12-10 18:39:31 -0800

    UCSFD8-881 reading progress bar for articles

---
+++ docroot/themes/custom/ucsf/assets/js/ucsf_reading_progress_bar.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function ($) {
+
+  // Wait for the document to be ready.
+  $(function () {
+    // The progress bar which will be updated on scroll.
+    var progressBar = document.querySelector('.progress-bar');
+
+    // The article used to detect scroll position.
+    var article = document.querySelector('.main-content');
+
+    // Get the Y position for where the article sits on the page.
+    var rect = article.getBoundingClientRect();
+    var articleTop = rect.top + document.documentElement.scrollTop;
+
+    // Get the length of the article minus half of the screen height so that
+    // an article will be completed reading once the bottom edge of it is half
+    // way up the screen.
+    var screenHalfHeight = window.innerHeight / 2;
+    var articleHeight = article.clientHeight - screenHalfHeight;
+
+    window.addEventListener('scroll', function (e) {
+      // Get the current scroll position within the article.
+      var scrollY = window.scrollY - articleTop;
+      // Get the average progress ratio of article scrolled through.
+      var progress = scrollY / articleHeight;
+
+      // Ensure that the progress bar never goes lower than zero or higher than 1
+      if (progress > 1) {
+        progress = 1;
+      } else if (progress < 0) {
+        progress = 0;
+      }
+
+      // Set the progress bar percent.
+      progressBar.setAttribute('value', progress);
+    });
+  });
+})(jQuery);
+//# sourceMappingURL=ucsf_reading_progress_bar.js.map
---
+++ docroot/themes/custom/ucsf/js/ucsf_reading_progress_bar.js
@@ -0,0 +1,39 @@
+(($ => {
+
+  // Wait for the document to be ready.
+  $(() => {
+    // The progress bar which will be updated on scroll.
+    const progressBar = document.querySelector('.progress-bar');
+
+    // The article used to detect scroll position.
+    const article = document.querySelector('.main-content');
+
+    // Get the Y position for where the article sits on the page.
+    const rect = article.getBoundingClientRect();
+    const articleTop = rect.top + document.documentElement.scrollTop;
+
+    // Get the length of the article minus half of the screen height so that
+    // an article will be completed reading once the bottom edge of it is half
+    // way up the screen.
+    const screenHalfHeight = window.innerHeight / 2;
+    const articleHeight = article.clientHeight - screenHalfHeight;
+
+    window.addEventListener('scroll', function(e) {
+      // Get the current scroll position within the article.
+      const scrollY = window.scrollY - articleTop;
+      // Get the average progress ratio of article scrolled through.
+      let progress = scrollY / articleHeight;
+
+      // Ensure that the progress bar never goes lower than zero or higher than 1
+      if (progress > 1) {
+        progress = 1;
+      } else if (progress < 0) {
+        progress = 0;
+      }
+
+      // Set the progress bar percent.
+      progressBar.setAttribute('value', progress);
+    })
+
+  });
+}))(jQuery);
