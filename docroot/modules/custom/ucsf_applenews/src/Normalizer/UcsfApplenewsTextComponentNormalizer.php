35d59b6c82149a7531377a0f8b1de0a9db93522b
Sasha Miller <sashazm85@gmail.com>
2020-01-17 12:48:54 -0800

    UCSFD8-900 inline comments added

--- docroot/modules/custom/ucsf_applenews/src/Normalizer/UcsfApplenewsTextComponentNormalizer.php
+++ docroot/modules/custom/ucsf_applenews/src/Normalizer/UcsfApplenewsTextComponentNormalizer.php
@@ -602,6 +602,7 @@ class UcsfApplenewsTextComponentNormalizer extends ApplenewsTextComponentNormali
                 $video_field = $media->get('field_media_video_embed_field');
                 if ($url = $video_field->get(0)->getString()) {
                   $component = $this->getVideoComponent($url);
+                  // don't error if for some reason $component is NULL
                   if (!empty($component)) {
                     $component->setLayout(_ucsf_applenews_video_component_layout());
                   }
@@ -862,6 +863,7 @@ class UcsfApplenewsTextComponentNormalizer extends ApplenewsTextComponentNormali
    */
   protected function getVideoComponent($url) {
     $url_parsed = parse_url($url);
+    // RegEx for youtube.com, youtu.be, vimeo.com
     if (preg_match('/(vimeo\.com|youtu\.be|www\.youtube\.com)/', $url_parsed['host'])) {
       if (empty($url_parsed['scheme'])) {
         $url = 'https:' . $url;
