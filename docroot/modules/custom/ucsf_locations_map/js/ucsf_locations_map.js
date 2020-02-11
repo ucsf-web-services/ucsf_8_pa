9ee2c62fa49e12108727d55a04e50ffb53608d40
Sasha Miller <sashazm85@gmail.com>
2020-02-10 17:04:07 -0800

    UCSFD8-828 initial setup for jsonapi

--- docroot/modules/custom/ucsf_locations_map/js/ucsf_locations_map.js
+++ docroot/modules/custom/ucsf_locations_map/js/ucsf_locations_map.js
@@ -20,121 +20,215 @@
       var pagedata;
       pagedata = $("div.title").text();
 
-      //var nid = Drupal.settings.nid;
-      //you must double escape the newlines coming from the Google copy/paste so they look like this [ \\n ]
-      $.getJSON('/maps_api.php?name='+pagedata, function(data) {
-        //console.dir(data);
+      const url = '/jsonapi/node/map?include=field_map_locations&include=field_map_locations.field_location_images,field_map_locations.field_location_type&filter%5Btitle%5D='+pagedata
+      // $.getJSON(url, function(data) {
+      //   // Parsing JSON API object
+      //   const parcedData = window.jsonapi.parse(data);
+      //   // Array of location objects
+      //   const locations = parcedData.data[0].field_map_locations;
+      //   locations.forEach(location => {
+      //     // console.log(location);
+      //     const title = location.field_title;
+      //     console.log(title);
+      //     const address = {
+      //       street: location.field_address.address_line1,
+      //       locality: location.field_address.locality,
+      //       state: location.field_address.administrative_area,
+      //       postalCode: location.field_address.postal_code
+      //     }
+      //     console.log(address);
+          // const description = location.field_location_text.processed
+          // console.log(description);
+      //     const geofield = {
+      //       latitude: location.field_geofield.lat,
+      //       longitude: location.field_geofield.lon
+      //     }
+      //     console.log(geofield);
+
+      //     const images = location.field_location_images.map(image => {
+      //       return image.uri.url;
+      //     });
+      //     console.log(images);
+      //     const primaryImg = images[0];
+      //     console.log(primaryImg);
+
+      //     const locationType = location.field_location_type.name
+      //     console.log(locationType);
+
+      //     const website = location.field_location_link.uri
+      //     console.log(website);
+      //     // console.log(locations);
+      //   });
+      // })
+
+      $.getJSON(url, function(data) {
+        // Parsing JSON API object
+        const parcedData = window.jsonapi.parse(data);
+        // Array of location objects
         var locations = {};
         var infoWindows = [];
         var markers = [];
-        //console.dir(data);
-        $.each(data.nodes, function(index, n) {
-          var location = n.node;
-          if (!locations[location.Type]) {
-            locations[location.Type] = { indexes: [], list: [] }
+
+        const rawLocations = parcedData.data[0].field_map_locations;
+        rawLocations.forEach((location, index) => {
+          console.log(location);
+
+          const locationType = location.field_location_type.name
+          if (!locations[locationType]) {
+            locations[locationType] = { indexes: [], list: [] }
           }
-          locations[location.Type].list.push("<li id='loc-" + index + "'>" + location.Title + "</li>");
-          locations[location.Type].indexes.push(index);
-          //console.dir(location);
-          var geofield;
-          if (location.hasOwnProperty('Geofield')) {
-            geofield = location.Geofield.split(', ');
-          } else {
-            geofield = location.geofield.split(', ');
-          }
-          var icon = '';
-          if(location.Type == 'Parking') {
-            // icon = 'https://www.ucsf.edu/sites/default/files/parking-icon-01.png'
+          locations[locationType].list.push("<li id='loc-" + index + "'>" + location.field_title + "</li>");
+          locations[locationType].indexes.push(index);
+          let icon = '';
+          if (locationType === 'Parking') {
             icon = {
               url: 'https://www.ucsf.edu/sites/default/files/parking-icon-01.png',
               scaledSize: new google.maps.Size(28, 32)
             };
           }
-          if(location.Body == null) {
-            location.Body = '';
-          }
           markers[index] = new google.maps.Marker({
             map: map,
-            position: { lat: parseFloat(geofield[0]), lng: parseFloat(geofield[1]) },
-            title: location.Title,
+            position: { lat: parseFloat(location.field_geofield.lat), lng: parseFloat(location.field_geofield.lon) },
+            title: location.field_title,
             icon: icon
           });
 
           bounds.extend(markers[index].position);
-
           var imagesMarkup = '<div class="map-images">';
-          if ($.isArray(location.Images)) {
-            $.each(location.Images, function(index, img) {
-              imagesMarkup += '<div class="map-image"><img src="' + img.src + '" /></div>';
-            })
-          } else if (location.Images != '' && location.Images != null) {
-            imagesMarkup += '<div class="map-image"><img src="' + location.Images + '" /></div>';
-          }
+          const images = location.field_location_images;
+          images.forEach((image, index ) => {
+            imagesMarkup += '<div class="map-image"><img src="' + image.uri.url + '" /></div>';
+          })
           imagesMarkup += '</div>';
 
           infoWindows[index] = new google.maps.InfoWindow({
-            content: "<div><h3>" + location.Title + "</h3><div class='body'>" + location.Body +
-            "</div><p class='address'>" + location.Address + "</p>" +
-            "<a class='directions' href='https://www.google.com/maps/dir/Current+Location/" + geofield[0] + "," + geofield[1] +
-            "'>Directions</a>" + (location.Link ? "<br/><a class='website' href='" + location.Link + "'>Website</a>" : "") + imagesMarkup + "</div>",
+            content: "<div><h3>" + location.field_title + "</h3><div class='body'>" + location.field_location_text.processed +
+            "</div><p class='address'>" + location.field_address.address_line1 + "<br>" + location.field_address.locality + " " + location.field_address.administrative_area + " " + location.field_address.postal_code + "</p>" +
+            "<a class='directions' href='https://www.google.com/maps/dir/Current+Location/" + location.field_geofield.lat + "," + location.field_geofield.lon +
+            "'>Directions</a>" + (location.field_location_link.uri ? "<br/><a class='website' href='" + location.field_location_link.uri + "'>Website</a>" : "") + imagesMarkup + "</div>",
             maxWidth: 310
           })
+          console.log(infoWindows[index].content);
+
         });
-
-        map.fitBounds(bounds);
-
-        var markup = '';
-
-        $.each(locations, function(type, l) {
-          markup += '<div><input id="' + type + '" type="checkbox" checked="checked" /><label for="' + type + '"><h3>' + type + '</h3></label><ul>' + l.list.join('') + '</ul></div>'
-
-          $('body').on('click', "label[for='" + type + "']", function (){
-            var input = $('input#' + type);
-
-            if (input.is(':checked')) {
-              input.nextAll('ul').hide('fast');
-              $.each(locations[type].indexes, function(i, index) {
-                infoWindows[index].close();
-                markers[index].setVisible(false)
-              })
-            } else {
-              input.nextAll('ul').show('fast');
-              $.each(locations[type].indexes, function(i, index) {
-                infoWindows[index].close();
-                markers[index].setVisible(true)
-              })
-            }
-          })
-        });
-        //pageTitle =  $('#locations-list .title').html();
-
-        $('#locations-list .loading').html(markup);
-
-        $.each(data.nodes, function(index, n) {
-          google.maps.event.addDomListener(document.getElementById('loc-' + index), 'click', (function () {
-            $.each(infoWindows, function(index, infoWindow) {
-              infoWindow.close()
-            });
-            infoWindows[index].open(map, markers[index])
-          }));
-
-          google.maps.event.addListener(markers[index], 'click', (function () {
-            $.each(infoWindows, function(index, infoWindow) {
-              infoWindow.close()
-            });
-            infoWindows[index].open(map, markers[index])
-          }))
-        });
-
-        var center;
-        google.maps.event.addDomListener(map, 'idle', function() {
-          center = map.getCenter()
-        });
-        google.maps.event.addDomListener(window, 'resize', function() {
-          google.maps.event.trigger(map, 'resize');
-          map.setCenter(center);
-        })
       })
+
+
+      //var nid = Drupal.settings.nid;
+      //you must double escape the newlines coming from the Google copy/paste so they look like this [ \\n ]
+      // $.getJSON('/maps_api.php?name='+pagedata, function(data) {
+      //   //console.dir(data);
+      //   var locations = {};
+      //   var infoWindows = [];
+      //   var markers = [];
+      //   //console.dir(data);
+      //   $.each(data.nodes, function(index, n) {
+      //     var location = n.node;
+      //     if (!locations[location.Type]) {
+      //       locations[location.Type] = { indexes: [], list: [] }
+      //     }
+      //     locations[location.Type].list.push("<li id='loc-" + index + "'>" + location.Title + "</li>");
+      //     locations[location.Type].indexes.push(index);
+      //     // console.dir(location);
+      //     var geofield;
+      //     if (location.hasOwnProperty('Geofield')) {
+      //       geofield = location.Geofield.split(', ');
+      //     } else {
+      //       geofield = location.geofield.split(', ');
+      //     }
+      //     var icon = '';
+      //     if(location.Type == 'Parking') {
+      //       // icon = 'https://www.ucsf.edu/sites/default/files/parking-icon-01.png'
+      //       icon = {
+      //         url: 'https://www.ucsf.edu/sites/default/files/parking-icon-01.png',
+      //         scaledSize: new google.maps.Size(28, 32)
+      //       };
+      //     }
+      //     if(location.Body == null) {
+      //       location.Body = '';
+      //     }
+      //     markers[index] = new google.maps.Marker({
+      //       map: map,
+      //       position: { lat: parseFloat(geofield[0]), lng: parseFloat(geofield[1]) },
+      //       title: location.Title,
+      //       icon: icon
+      //     });
+
+      //     bounds.extend(markers[index].position);
+
+      //     var imagesMarkup = '<div class="map-images">';
+      //     if ($.isArray(location.Images)) {
+      //       $.each(location.Images, function(index, img) {
+      //         imagesMarkup += '<div class="map-image"><img src="' + img.src + '" /></div>';
+      //       })
+      //     } else if (location.Images != '' && location.Images != null) {
+      //       imagesMarkup += '<div class="map-image"><img src="' + location.Images + '" /></div>';
+      //     }
+      //     imagesMarkup += '</div>';
+
+      //     infoWindows[index] = new google.maps.InfoWindow({
+      //       content: "<div><h3>" + location.Title + "</h3><div class='body'>" + location.Body +
+      //       "</div><p class='address'>" + location.Address + "</p>" +
+      //       "<a class='directions' href='https://www.google.com/maps/dir/Current+Location/" + geofield[0] + "," + geofield[1] +
+      //       "'>Directions</a>" + (location.Link ? "<br/><a class='website' href='" + location.Link + "'>Website</a>" : "") + imagesMarkup + "</div>",
+      //       maxWidth: 310
+      //     })
+      //   });
+
+      //   map.fitBounds(bounds);
+
+      //   var markup = '';
+
+      //   $.each(locations, function(type, l) {
+      //     markup += '<div><input id="' + type + '" type="checkbox" checked="checked" /><label for="' + type + '"><h3>' + type + '</h3></label><ul>' + l.list.join('') + '</ul></div>'
+
+      //     $('body').on('click', "label[for='" + type + "']", function (){
+      //       var input = $('input#' + type);
+
+      //       if (input.is(':checked')) {
+      //         input.nextAll('ul').hide('fast');
+      //         $.each(locations[type].indexes, function(i, index) {
+      //           infoWindows[index].close();
+      //           markers[index].setVisible(false)
+      //         })
+      //       } else {
+      //         input.nextAll('ul').show('fast');
+      //         $.each(locations[type].indexes, function(i, index) {
+      //           infoWindows[index].close();
+      //           markers[index].setVisible(true)
+      //         })
+      //       }
+      //     })
+      //   });
+      //   //pageTitle =  $('#locations-list .title').html();
+
+      //   $('#locations-list .loading').html(markup);
+
+      //   $.each(data.nodes, function(index, n) {
+      //     google.maps.event.addDomListener(document.getElementById('loc-' + index), 'click', (function () {
+      //       $.each(infoWindows, function(index, infoWindow) {
+      //         infoWindow.close()
+      //       });
+      //       infoWindows[index].open(map, markers[index])
+      //     }));
+
+      //     google.maps.event.addListener(markers[index], 'click', (function () {
+      //       $.each(infoWindows, function(index, infoWindow) {
+      //         infoWindow.close()
+      //       });
+      //       infoWindows[index].open(map, markers[index])
+      //     }))
+      //   });
+        // console.log(infoWindows)
+        // var center;
+        // google.maps.event.addDomListener(map, 'idle', function() {
+        //   center = map.getCenter()
+        // });
+        // google.maps.event.addDomListener(window, 'resize', function() {
+        //   google.maps.event.trigger(map, 'resize');
+        //   map.setCenter(center);
+        // })
+      // })
     }
   }
 })(jQuery, Drupal, drupalSettings);
