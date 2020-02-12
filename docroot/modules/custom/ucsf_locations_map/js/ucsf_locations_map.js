30b51b6170aa4ff9489709bea70cec338711d09a
Sasha Miller <sashazm85@gmail.com>
2020-02-11 16:53:01 -0800

    UCSFD8-828 only execute the js once

--- docroot/modules/custom/ucsf_locations_map/js/ucsf_locations_map.js
+++ docroot/modules/custom/ucsf_locations_map/js/ucsf_locations_map.js
@@ -3,167 +3,172 @@
  * Scripts for UCSF dom
  *
  */
-(function ($, Drupal, drupalSettings) {
+(function ($, Drupal, drupalSettings, window) {
 
   Drupal.behaviors.map = {
     attach: function (context, settings) {
-      // Google map set up and center for the map
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 37.195, lng: -123.775 },
-        scrollwheel: false,
-        zoom: 4
-      });
-      // Geographical area to be included in the map
-      var bounds = new google.maps.LatLngBounds();
 
-      //getting the node title for the url from which jsonapi pulls the data
-      var pagedata;
-      pagedata = $("div.title").text();
-      var url = '/jsonapi/node/map?include=field_map_locations&include=field_map_locations.field_location_images,field_map_locations.field_location_type&filter%5Btitle%5D=' + pagedata
+      // Only run this code once.
+      $(window, context).once().each( function() {
 
-      $.getJSON(url, function(data) {
-        // Parsing JSON API object
+        // Google map set up and center for the map
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: { lat: 37.195, lng: -123.775 },
+          scrollwheel: false,
+          zoom: 4
+        });
+        // Geographical area to be included in the map
+        var bounds = new google.maps.LatLngBounds();
 
-        var parcedData = window.jsonapi.parse(data);
-        // Variables for holding location information.
-        var locations = {};
-        var infoWindows = [];
-        var markers = [];
+        //getting the node title for the url from which jsonapi pulls the data
+        var pagedata = $("div.title").text();
+        var url = '/jsonapi/node/map?include=field_map_locations&include=field_map_locations.field_location_images,field_map_locations.field_location_type&filter%5Btitle%5D=' + pagedata
 
-        // Array of map location objects
-        var rawLocations = purgeLocations(parcedData.data[0].field_map_locations);
+        $.getJSON(url, function(data) {
+          // Parsing JSON API object
 
-        // Looping through each location object in the array,
-        // creating HTML markup with data provided in the array.
-        $.each(rawLocations, function(index, location) {
-          //Location type: Building or Parking.
-          var locationType = location.field_location_type.name
+          var parcedData = window.jsonapi.parse(data);
+          // Variables for holding location information.
+          var locations = {};
+          var infoWindows = [];
+          var markers = [];
 
-          //  Initial state, before type is selected.
-          if (!locations[locationType]) {
-            locations[locationType] = { indexes: [], list: [] }
-          }
-          // Generate a list item for location type,
-          // inside the list item display building's type (example: campus, main building...).
-          locations[locationType].list.push("<li id='loc-" + index + "'>" + location.field_title + "</li>");
-          locations[locationType].indexes.push(index);
-          var icon = '';
+          // Array of map location objects
+          var rawLocations = purgeLocations(parcedData.data[0].field_map_locations);
 
-          // Change icon for Parking location type.
-          if (locationType === 'Parking') {
-            icon = {
-              url: 'https://www.ucsf.edu/sites/default/files/parking-icon-01.png',
-              scaledSize: new google.maps.Size(28, 32)
-            };
-          }
-          // Generate a marker for the location using coordinates, on hover title (building's type), icon.
-          markers[index] = new google.maps.Marker({
-            map: map,
-            position: { lat: parseFloat(location.field_geofield.lat), lng: parseFloat(location.field_geofield.lon) },
-            title: location.field_title,
-            icon: icon
+          // Looping through each location object in the array,
+          // creating HTML markup with data provided in the array.
+          $.each(rawLocations, function(index, location) {
+            //Location type: Building or Parking.
+            var locationType = location.field_location_type.name
+
+            //  Initial state, before type is selected.
+            if (!locations[locationType]) {
+              locations[locationType] = { indexes: [], list: [] }
+            }
+            // Generate a list item for location type,
+            // inside the list item display building's type (example: campus, main building...).
+            locations[locationType].list.push("<li id='loc-" + index + "'>" + location.field_title + "</li>");
+            locations[locationType].indexes.push(index);
+            var icon = '';
+
+            // Change icon for Parking location type.
+            if (locationType === 'Parking') {
+              icon = {
+                url: 'https://www.ucsf.edu/sites/default/files/parking-icon-01.png',
+                scaledSize: new google.maps.Size(28, 32)
+              };
+            }
+            // Generate a marker for the location using coordinates, on hover title (building's type), icon.
+            markers[index] = new google.maps.Marker({
+              map: map,
+              position: { lat: parseFloat(location.field_geofield.lat), lng: parseFloat(location.field_geofield.lon) },
+              title: location.field_title,
+              icon: icon
+            });
+
+            // Extend the map so that marker location is visible
+            bounds.extend(markers[index].position);
+
+            // Address.
+            var address = '';
+            if (location.field_address) {
+              address = location.field_address.address_line1 + "<br>" + location.field_address.locality + " " + location.field_address.administrative_area + " " + location.field_address.postal_code;
+            }
+
+            // Website Link.
+            var link = '';
+            if (location.field_location_link) {
+              link = (location.field_location_link.uri ? "<a class='website' href='" + location.field_location_link.uri + "'>Website</a>" : "")
+            }
+
+            // Array of image objects for current location
+            var images = location.field_location_images;
+
+            // Opening tag for group of images wrapper.
+            var imagesMarkup = '<div class="map-images">';
+            // loop through all of the images and generate HTML markup for each.
+            $.each(images, function(index, image) {
+              imagesMarkup += '<div class="map-image"><img src="' + image.uri.url + '" alt="' + image.meta.alt + '" /></div>';
+            })
+            // Closing tag for group of images wrapper.
+            imagesMarkup += '</div>';
+
+            // Pop up window above location marker
+            // Displays: Building's Title, Description, Address, Link to driving directions, Website, Location Images,
+            infoWindows[index] = new google.maps.InfoWindow({
+              content: "<div class='infobox'>" +
+              "<h3>" + location.field_title + "</h3><div class='body'>" + location.field_location_text.processed + "</div>" +
+              "<p class='address'>" + address + "</p>" +
+              "<div class='link'>" +
+              "<a class='directions' href='https://www.google.com/maps/dir/Current+Location/" + location.field_geofield.lat + "," + location.field_geofield.lon + "'>Directions</a>" +
+              link +
+              "</div>" +
+              imagesMarkup +
+              "</div>",
+              // maximup width of the pop up.
+              maxWidth: 266
+            })
+          });
+          // Sets the viewport to contain the given bounds.
+          // so that pop up for the selected marker fits on screen?
+          map.fitBounds(bounds);
+
+          var markup = '';
+          // Checkbox to show / hide the list of locations by type.
+          $.each(locations, function(type, item) {
+            markup += '<div><input id="' + type + '" type="checkbox" checked="checked" /><label for="' + type + '"><h3>' + type + '</h3></label><ul>' + item.list.join('') + '</ul></div>'
+            // Event handler to capture the click hide / show list items for the checked checkbox category.
+            $('body').on('click', "label[for='" + type + "']", function (){
+              var input = $('input#' + type);
+
+              if (input.is(':checked')) {
+                input.nextAll('ul').hide('fast');
+                $.each(locations[type].indexes, function(index, value) {
+                  infoWindows[value].close();
+                  markers[value].setVisible(false)
+                })
+              } else {
+                input.nextAll('ul').show('fast');
+                $.each(locations[type].indexes, function(index, value) {
+                  infoWindows[value].close();
+                  markers[value].setVisible(true)
+                })
+              }
+            })
+          });
+          // Replace initial loading message with generated markup.
+          $('#locations-list .loading').html(markup);
+
+          // Close previous location marker pop up when new location is selected.
+          $.each(rawLocations, function(index, location) {
+            google.maps.event.addDomListener(document.getElementById('loc-' + index), 'click', (function () {
+              $.each(infoWindows, function(index, infoWindow) {
+                infoWindow.close()
+              });
+              infoWindows[index].open(map, markers[index])
+            }));
+
+            google.maps.event.addListener(markers[index], 'click', (function () {
+              $.each(infoWindows, function(index, infoWindow) {
+                infoWindow.close()
+              });
+              infoWindows[index].open(map, markers[index])
+            }))
           });
 
-          // Extend the map so that marker location is visible
-          bounds.extend(markers[index].position);
-
-          // Address.
-          var address = '';
-          if (location.field_address) {
-            address = location.field_address.address_line1 + "<br>" + location.field_address.locality + " " + location.field_address.administrative_area + " " + location.field_address.postal_code;
-          }
-
-          // Website Link.
-          var link = '';
-          if (location.field_location_link) {
-            link = (location.field_location_link.uri ? "<a class='website' href='" + location.field_location_link.uri + "'>Website</a>" : "")
-          }
-
-          // Array of image objects for current location
-          var images = location.field_location_images;
-
-          // Opening tag for group of images wrapper.
-          var imagesMarkup = '<div class="map-images">';
-          // loop through all of the images and generate HTML markup for each.
-          $.each(images, function(index, image) {
-            imagesMarkup += '<div class="map-image"><img src="' + image.uri.url + '" alt="' + image.meta.alt + '" /></div>';
+          // adjust the map center on screen resize.
+          var center;
+          google.maps.event.addDomListener(map, 'idle', function() {
+            center = map.getCenter()
+          });
+          google.maps.event.addDomListener(window, 'resize', function() {
+            google.maps.event.trigger(map, 'resize');
+            map.setCenter(center);
           })
-          // Closing tag for group of images wrapper.
-          imagesMarkup += '</div>';
-
-          // Pop up window above location marker
-          // Displays: Building's Title, Description, Address, Link to driving directions, Website, Location Images,
-          infoWindows[index] = new google.maps.InfoWindow({
-            content: "<div class='infobox'>" +
-            "<h3>" + location.field_title + "</h3><div class='body'>" + location.field_location_text.processed + "</div>" +
-            "<p class='address'>" + address + "</p>" +
-            "<div class='link'>" +
-            "<a class='directions' href='https://www.google.com/maps/dir/Current+Location/" + location.field_geofield.lat + "," + location.field_geofield.lon + "'>Directions</a>" +
-            link +
-            "</div>" +
-            imagesMarkup +
-            "</div>",
-            // maximup width of the pop up.
-            maxWidth: 266
-          })
-        });
-        // Sets the viewport to contain the given bounds.
-        // so that pop up for the selected marker fits on screen?
-        map.fitBounds(bounds);
-
-        var markup = '';
-        // Checkbox to show / hide the list of locations by type.
-        $.each(locations, function(type, item) {
-          markup += '<div><input id="' + type + '" type="checkbox" checked="checked" /><label for="' + type + '"><h3>' + type + '</h3></label><ul>' + item.list.join('') + '</ul></div>'
-          // Event handler to capture the click hide / show list items for the checked checkbox category.
-          $('body').on('click', "label[for='" + type + "']", function (){
-            var input = $('input#' + type);
-
-            if (input.is(':checked')) {
-              input.nextAll('ul').hide('fast');
-              $.each(locations[type].indexes, function(index, value) {
-                infoWindows[value].close();
-                markers[value].setVisible(false)
-              })
-            } else {
-              input.nextAll('ul').show('fast');
-              $.each(locations[type].indexes, function(index, value) {
-                infoWindows[value].close();
-                markers[value].setVisible(true)
-              })
-            }
-          })
-        });
-        // Replace initial loading message with generated markup.
-        $('#locations-list .loading').html(markup);
-
-        // Close previous location marker pop up when new location is selected.
-        $.each(rawLocations, function(index, location) {
-          google.maps.event.addDomListener(document.getElementById('loc-' + index), 'click', (function () {
-            $.each(infoWindows, function(index, infoWindow) {
-              infoWindow.close()
-            });
-            infoWindows[index].open(map, markers[index])
-          }));
-
-          google.maps.event.addListener(markers[index], 'click', (function () {
-            $.each(infoWindows, function(index, infoWindow) {
-              infoWindow.close()
-            });
-            infoWindows[index].open(map, markers[index])
-          }))
-        });
-
-        // adjust the map center on screen resize.
-        var center;
-        google.maps.event.addDomListener(map, 'idle', function() {
-          center = map.getCenter()
-        });
-        google.maps.event.addDomListener(window, 'resize', function() {
-          google.maps.event.trigger(map, 'resize');
-          map.setCenter(center);
         })
-      })
+
+      });
     }
   }
 
@@ -188,4 +193,4 @@
 
     return locations;
   }
-})(jQuery, Drupal, drupalSettings);
+})(jQuery, Drupal, drupalSettings, window);
