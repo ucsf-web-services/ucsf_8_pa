ec9adbcb14f50d4b3344086a4c5d3c390ff03fba
Sasha Miller <sashazm85@gmail.com>
2020-02-11 16:39:07 -0800

    UCSFD8-828 code cleanup

--- docroot/modules/custom/ucsf_locations_map/js/ucsf_locations_map.js
+++ docroot/modules/custom/ucsf_locations_map/js/ucsf_locations_map.js
@@ -7,85 +7,56 @@
 
   Drupal.behaviors.map = {
     attach: function (context, settings) {
-
+      // Google map set up and center for the map
       var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 37.195, lng: -123.775 },
         scrollwheel: false,
         zoom: 4
       });
-
+      // Geographical area to be included in the map
       var bounds = new google.maps.LatLngBounds();
 
-      //getting the node title to pass to maps_api.php instead of using nid
+      //getting the node title for the url from which jsonapi pulls the data
       var pagedata;
       pagedata = $("div.title").text();
-
-      const url = '/jsonapi/node/map?include=field_map_locations&include=field_map_locations.field_location_images,field_map_locations.field_location_type&filter%5Btitle%5D='+pagedata
-      // $.getJSON(url, function(data) {
-      //   // Parsing JSON API object
-      //   const parcedData = window.jsonapi.parse(data);
-      //   // Array of location objects
-      //   const locations = parcedData.data[0].field_map_locations;
-      //   locations.forEach(location => {
-      //     // console.log(location);
-      //     const title = location.field_title;
-      //     console.log(title);
-      //     const address = {
-      //       street: location.field_address.address_line1,
-      //       locality: location.field_address.locality,
-      //       state: location.field_address.administrative_area,
-      //       postalCode: location.field_address.postal_code
-      //     }
-      //     console.log(address);
-          // const description = location.field_location_text.processed
-          // console.log(description);
-      //     const geofield = {
-      //       latitude: location.field_geofield.lat,
-      //       longitude: location.field_geofield.lon
-      //     }
-      //     console.log(geofield);
-
-      //     const images = location.field_location_images.map(image => {
-      //       return image.uri.url;
-      //     });
-      //     console.log(images);
-      //     const primaryImg = images[0];
-      //     console.log(primaryImg);
-
-      //     const locationType = location.field_location_type.name
-      //     console.log(locationType);
-
-      //     const website = location.field_location_link.uri
-      //     console.log(website);
-      //     // console.log(locations);
-      //   });
-      // })
+      var url = '/jsonapi/node/map?include=field_map_locations&include=field_map_locations.field_location_images,field_map_locations.field_location_type&filter%5Btitle%5D=' + pagedata
 
       $.getJSON(url, function(data) {
         // Parsing JSON API object
-        const parcedData = window.jsonapi.parse(data);
-        // Array of location objects
+
+        var parcedData = window.jsonapi.parse(data);
+        // Variables for holding location information.
         var locations = {};
         var infoWindows = [];
         var markers = [];
 
-        const rawLocations = parcedData.data[0].field_map_locations;
-        rawLocations.forEach((location, index) => {
-          console.log(location);
+        // Array of map location objects
+        var rawLocations = purgeLocations(parcedData.data[0].field_map_locations);
 
-          const locationType = location.field_location_type.name
+        // Looping through each location object in the array,
+        // creating HTML markup with data provided in the array.
+        $.each(rawLocations, function(index, location) {
+          //Location type: Building or Parking.
+          var locationType = location.field_location_type.name
+
+          //  Initial state, before type is selected.
           if (!locations[locationType]) {
             locations[locationType] = { indexes: [], list: [] }
           }
+          // Generate a list item for location type,
+          // inside the list item display building's type (example: campus, main building...).
           locations[locationType].list.push("<li id='loc-" + index + "'>" + location.field_title + "</li>");
           locations[locationType].indexes.push(index);
-          let icon = '';
+          var icon = '';
+
+          // Change icon for Parking location type.
           if (locationType === 'Parking') {
             icon = {
               url: 'https://www.ucsf.edu/sites/default/files/parking-icon-01.png',
               scaledSize: new google.maps.Size(28, 32)
             };
           }
+          // Generate a marker for the location using coordinates, on hover title (building's type), icon.
           markers[index] = new google.maps.Marker({
             map: map,
             position: { lat: parseFloat(location.field_geofield.lat), lng: parseFloat(location.field_geofield.lon) },
@@ -93,31 +64,58 @@
             icon: icon
           });
 
+          // Extend the map so that marker location is visible
           bounds.extend(markers[index].position);
+
+          // Address.
+          var address = '';
+          if (location.field_address) {
+            address = location.field_address.address_line1 + "<br>" + location.field_address.locality + " " + location.field_address.administrative_area + " " + location.field_address.postal_code;
+          }
+
+          // Website Link.
+          var link = '';
+          if (location.field_location_link) {
+            link = (location.field_location_link.uri ? "<a class='website' href='" + location.field_location_link.uri + "'>Website</a>" : "")
+          }
+
+          // Array of image objects for current location
+          var images = location.field_location_images;
+
+          // Opening tag for group of images wrapper.
           var imagesMarkup = '<div class="map-images">';
-          const images = location.field_location_images;
-          images.forEach((image, index ) => {
-            imagesMarkup += '<div class="map-image"><img src="' + image.uri.url + '" /></div>';
+          // loop through all of the images and generate HTML markup for each.
+          $.each(images, function(index, image) {
+            imagesMarkup += '<div class="map-image"><img src="' + image.uri.url + '" alt="' + image.meta.alt + '" /></div>';
           })
+          // Closing tag for group of images wrapper.
           imagesMarkup += '</div>';
 
+          // Pop up window above location marker
+          // Displays: Building's Title, Description, Address, Link to driving directions, Website, Location Images,
           infoWindows[index] = new google.maps.InfoWindow({
-            content: "<div><h3>" + location.field_title + "</h3><div class='body'>" + location.field_location_text.processed +
-            "</div><p class='address'>" + location.field_address.address_line1 + "<br>" + location.field_address.locality + " " + location.field_address.administrative_area + " " + location.field_address.postal_code + "</p>" +
-            "<a class='directions' href='https://www.google.com/maps/dir/Current+Location/" + location.field_geofield.lat + "," + location.field_geofield.lon +
-            "'>Directions</a>" + (location.field_location_link.uri ? "<br/><a class='website' href='" + location.field_location_link.uri + "'>Website</a>" : "") + imagesMarkup + "</div>",
-            maxWidth: 310
+            content: "<div class='infobox'>" +
+            "<h3>" + location.field_title + "</h3><div class='body'>" + location.field_location_text.processed + "</div>" +
+            "<p class='address'>" + address + "</p>" +
+            "<div class='link'>" +
+            "<a class='directions' href='https://www.google.com/maps/dir/Current+Location/" + location.field_geofield.lat + "," + location.field_geofield.lon + "'>Directions</a>" +
+            link +
+            "</div>" +
+            imagesMarkup +
+            "</div>",
+            // maximup width of the pop up.
+            maxWidth: 266
           })
-          console.log(infoWindows[index].content);
         });
-
+        // Sets the viewport to contain the given bounds.
+        // so that pop up for the selected marker fits on screen?
         map.fitBounds(bounds);
 
         var markup = '';
-
+        // Checkbox to show / hide the list of locations by type.
         $.each(locations, function(type, item) {
           markup += '<div><input id="' + type + '" type="checkbox" checked="checked" /><label for="' + type + '"><h3>' + type + '</h3></label><ul>' + item.list.join('') + '</ul></div>'
-
+          // Event handler to capture the click hide / show list items for the checked checkbox category.
           $('body').on('click', "label[for='" + type + "']", function (){
             var input = $('input#' + type);
 
@@ -136,9 +134,10 @@
             }
           })
         });
-
+        // Replace initial loading message with generated markup.
         $('#locations-list .loading').html(markup);
 
+        // Close previous location marker pop up when new location is selected.
         $.each(rawLocations, function(index, location) {
           google.maps.event.addDomListener(document.getElementById('loc-' + index), 'click', (function () {
             $.each(infoWindows, function(index, infoWindow) {
@@ -154,7 +153,8 @@
             infoWindows[index].open(map, markers[index])
           }))
         });
-        console.log(infoWindows)
+
+        // adjust the map center on screen resize.
         var center;
         google.maps.event.addDomListener(map, 'idle', function() {
           center = map.getCenter()
@@ -165,5 +165,27 @@
         })
       })
     }
+  }
+
+  /**
+   * Remove any items from the array that do not have required fields.
+   *
+   * @param {array} locations
+   */
+  function purgeLocations(locations) {
+    var remove = [];
+    $.each(locations, function(index, location) {
+      // add the location to be removed if there are not required fields.
+      if (!location.field_title || !location.field_geofield || !location.field_location_type) {
+        remove.push(index);
+      }
+    });
+
+    // Loop throught the raw locations and remove any set to be removed.
+    for (var i = remove.length -1; i >= 0; i--) {
+      locations.splice(remove[i], 1);
+    }
+
+    return locations;
   }
 })(jQuery, Drupal, drupalSettings);
