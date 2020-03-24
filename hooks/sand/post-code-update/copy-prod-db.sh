b64b8bf243785cc11b558f4ea8bb3dd718dfa7b9
Sasha Miller <sashazm85@gmail.com>
2020-03-24 14:00:04 -0700

    Copy prod DB to Sand on code commit

--- hooks/sand/post-code-update/copy-prod-db.sh
+++ hooks/sand/post-code-update/copy-prod-db.sh
@@ -8,12 +8,12 @@ echo "Starting the Database copy from Production."
 
 # Map the script inputs to convenient names.
 site=$1
-sand_env=$2
-sand_alias=$site'.'$target_env
+sand_alias=$site'.sand'
 prod_alias=$site'.prod'
 
 echo "Site: $site"
-echo "Sand Env: $target_env"
+echo "Sand Env: $sand_alias"
 echo "Sand Drush Alias: $sand_alias"
+echo "Prod Drush Alias: $prod_alias"
 
 drush sql-sync @$prod_alias @$sand_alias
