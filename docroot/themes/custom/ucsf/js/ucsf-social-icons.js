5dd0fe8210f40fe8febf9f8f1bec2e18d4f8c2cb
Sasha Miller <sashazm85@gmail.com>
2020-04-27 14:24:45 -0700

    UCSFD8-955 debug

--- docroot/themes/custom/ucsf/js/ucsf-social-icons.js
+++ docroot/themes/custom/ucsf/js/ucsf-social-icons.js
@@ -1,25 +1,53 @@
 (($) => {
+  // All of this code is used for small screen:
   Drupal.behaviors.socialIconsMobile = {
     attach: function attach(context, settings) {
       $(window, context).once('social-icon-context').each(function () {
-
         const $socialIcons = $('.article-meta-share', context);
         const $iconPrint = $('li', context).has('a[title="Print Article"]');
         let lastScroll = 0;
 
-        // Custom Social Navbar behavior for mobile
+        // Social Navbar behavior
         const hideAndShow = () => {
           const currentScroll = window.pageYOffset;
           if (currentScroll > lastScroll) {
-            // If scrolling down hide icons
+            // If scrolling down, hide icons
             $socialIcons.removeClass('is-visible');
           } else {
-            // If scrolling up show icons
+            // If scrolling up, show icons
             $socialIcons.addClass('is-visible');
           }
 
           // Update scroll value
           lastScroll = currentScroll;
+        };
+
+        // Detect if article is not in viewport.
+        const  articleDetect = () => {
+          if ('IntersectionObserver' in window) {
+            // Article content wrapper
+            const hasSocialNav = document.querySelectorAll('#block-ucsf-content');
+
+            const observer = new IntersectionObserver(entries => {
+              entries.forEach(entry => {
+                // Article content is in viewport
+                if (entry.intersectionRatio > 0) {
+                  // show icons on up scroll
+                  window.addEventListener('scroll', hideAndShow);
+                } else {
+                  // Article content is not in viewport
+                  // hide icons and don't trigger the up scroll event.
+                  $socialIcons.removeClass('is-visible');
+                  window.removeEventListener('scroll', hideAndShow);
+                }
+              })
+            })
+
+            // Check if specified element is in viewport
+            hasSocialNav.forEach(element => {
+              observer.observe(element);
+            });
+          }
         };
 
         // Only use custom Social Navbar up scroll behavior in mobile
@@ -29,35 +57,7 @@
             // Remove "print" icon
             $iconPrint.css('display', 'none');
             window.addEventListener('scroll', hideAndShow);
-
-            // Social Navbar should not appear if article is not in viewport.
-            (function () {
-              if ('IntersectionObserver' in window) {
-                // Elements where Social Navbar should be visible
-                const hasSocialNav = document.querySelectorAll('#block-ucsf-content');
-
-                // Determine if element with Social Navbar is in viewport
-                const observer = new IntersectionObserver(entries => {
-                  entries.forEach(entry => {
-                    // Element with Social Navbar is in viewport
-                    if (entry.intersectionRatio > 0) {
-                      // show icons on up scroll
-                      window.addEventListener('scroll', hideAndShow);
-                    } else {
-                      // Element with Social Navbar is not in viewport
-                      // hide icons and don't trigger the up scroll event.
-                      $socialIcons.removeClass('is-visible');
-                      window.removeEventListener('scroll', hideAndShow);
-                    }
-                  })
-                })
-
-                  hasSocialNav.forEach(element => {
-                  observer.observe(element);
-                });
-              }
-
-            })();
+            articleDetect();
 
             // Desktop
           } else {
@@ -68,7 +68,7 @@
           }
         };
 
-        // Use MatchMedia to ensure that Social Navbar upscroll is only happening in mobile
+        // Use MatchMedia breakpoint to trigger small screen behavior and reset behavior for Desktop.
         const mql = matchMedia('(max-width: 1049px)');
         // Detect mobile on page load.
         mobileDetect(mql);
