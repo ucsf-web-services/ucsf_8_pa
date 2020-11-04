48634fd352b55efc61970c966e585e19ad0898af
Eric Guerin <eric.guerin@ucsf.edu>
2020-11-03 18:37:43 -0800

    BlankDiv style and alignment changes.

--- docroot/modules/custom/ucsf_wysiwyg/js/plugins/blankdiv/dialogs/blankdiv.js
+++ docroot/modules/custom/ucsf_wysiwyg/js/plugins/blankdiv/dialogs/blankdiv.js
@@ -13,10 +13,16 @@ CKEDITOR.dialog.add('blankdiv', function( editor ) {
             id: 'align',
             type: 'select',
             label: 'Align',
+            title: 'Align - set the elements container alignment.',
             items: [
               [ editor.lang.common.alignCenter, 'center' ],
               [ editor.lang.common.alignLeft, 'left' ],
-              [ editor.lang.common.alignRight, 'right' ]
+              [ editor.lang.common.alignRight, 'right' ],
+              ['Half Image Right', 'half-image-right'],
+              ['Half Image Left', 'half-image-left'],
+              ['Full Bleed Right', 'half-image-right-full'],
+              ['Full Bleed Left', 'half-image-left-full'],
+              ['Full Bleed',  'full-bleed-image' ]
             ],
             setup: function( widget ) {
               this.setValue( widget.data.align ? widget.data.align : 'right' );
@@ -28,15 +34,19 @@ CKEDITOR.dialog.add('blankdiv', function( editor ) {
           {
             id: 'size',
             type: 'select',
-            label: 'Size',
+            label: 'Style',
+            title: 'Style - Also known as Size, defines the size of the elements container.',
             items: [
-              [ 'Full Width', 'full' ],
-              [ 'Half Width', 'half' ],
-              [ 'Two Fifth', 'twofifth' ],
-              [ 'One Third', 'third' ]
+                  ['None (original image)', 'none'],
+                  ['WYSIWYG Full Bleed Half Image (680 x Y)', 'full_bleed_half__image'],
+                  ['WYSIWYG Full-Bleed Image (1280 x y)', 'full_bleed__image'],
+                  ['WYSIWYG Half Image (480 x Y)', 'half__image'],
+                  ['Quarter Image (220 x y)', 'quarter'],
+                  ['WYSIWYG Full Image (850 x y)', 'w'],
+                  ['WYSIWYG Callout square (150 x 150)', 'callout__image']
             ],
             setup: function( widget ) {
-              this.setValue( widget.data.size ? widget.data.size : 'twofifth' );
+              this.setValue( widget.data.size ? widget.data.size : 'none' );
             },
             commit: function( widget ) {
               widget.setData( 'size', this.getValue() );
@@ -58,3 +68,36 @@ CKEDITOR.dialog.add('blankdiv', function( editor ) {
     ]
   };
 } );
+
+
+/*
+
+OLD SETTINGS
+ //[ 'Full Width', 'full' ],
+ //[ 'Half Width', 'half' ],
+ //[ 'Two Fifth', 'twofifth' ],
+ //[ 'One Third', 'third' ]
+
+ Left
+ Center
+ Right
+ Half Image Right
+ Half Image Left
+ Full Bleed Right
+ Full Bleed Left
+ Full Bleed
+
+ 'half-image-right' => t('Half Image Right'),
+ 'half-image-left' => t('Half Image Left'),
+ 'half-image-right-full' => t('Full Bleed Right'),
+ 'half-image-left-full' => t('Full Bleed Left'),
+ 'full-bleed-image' => t('Full Bleed')
+
+ None (Original Image)
+ WYSIWYG Full Bleed Half Image (and 680 x Y)
+ WYSIWYG Full-Bleed Image (1280 x y)
+ WYSIWYG Half Image (480 x y)
+ Quarter Image (220 x y)
+ WYSIWYG Full Image (850 x y)
+ WYSIWYG Callout square (150 x 150)
+ */
--- docroot/modules/custom/ucsf_wysiwyg/js/plugins/blankdiv/plugin.js
+++ docroot/modules/custom/ucsf_wysiwyg/js/plugins/blankdiv/plugin.js
@@ -23,28 +23,49 @@ CKEDITOR.plugins.add('blankdiv', {
         return element.name == 'div' && element.hasClass('wysiwyg_blankdiv');
       },
       init: function() {
-        if ( this.element.hasClass( 'align--center' ) ) this.setData( 'align', 'center' );
-        if ( this.element.hasClass( 'align--left' ) ) this.setData( 'align', 'left' );
-        if ( this.element.hasClass( 'align--right' ) ) this.setData( 'align', 'right' );
-        if ( this.element.hasClass( 'size--full' ) ) this.setData( 'size', 'full' );
-        if ( this.element.hasClass( 'size--half' ) ) this.setData( 'size', 'half' );
-        if ( this.element.hasClass( 'size--twofifth' ) ) this.setData( 'size', 'twofifth' );
-        if ( this.element.hasClass( 'size--third' ) ) this.setData( 'size', 'third' );
+        // align options
+        if ( this.element.hasClass( 'center' ) ) this.setData( 'align', 'center' );
+        if ( this.element.hasClass( 'left' ) ) this.setData( 'align', 'left' );
+        if ( this.element.hasClass( 'right' ) ) this.setData( 'align', 'right' );
+        if ( this.element.hasClass( 'half-image-right' ) ) this.setData( 'align', 'half-image-right' );
+        if ( this.element.hasClass( 'half-image-left' ) ) this.setData( 'align', 'half-image-left' );
+        if ( this.element.hasClass( 'half-image-right-full' ) ) this.setData( 'align', 'half-image-right-full' );
+        if ( this.element.hasClass( 'half-image-left-full' ) ) this.setData( 'align', 'half-image-left-full' );
+        if ( this.element.hasClass( 'full-bleed-image' ) ) this.setData( 'align', 'full-bleed-image'  );
+        // size - style options
+        if ( this.element.hasClass( 'full_bleed_half__image' ) ) this.setData( 'size', 'full_bleed_half__image' );
+        if ( this.element.hasClass( 'full_bleed__image' ) ) this.setData( 'size', 'full_bleed__image' );
+        if ( this.element.hasClass( 'half__image' ) ) this.setData( 'size', 'half__image' );
+        if ( this.element.hasClass( 'quarter' ) ) this.setData( 'size', 'quarter' );
+        if ( this.element.hasClass( 'w' ) ) this.setData( 'size', 'w' );
+        if ( this.element.hasClass( 'wysiwyg_callout_square_150_x_150_' ) ) this.setData( 'size', 'wysiwyg_callout_square_150_x_150_' );
+
         if ( this.element.getHtml() ) this.setData( 'script', this.element.getHtml() );
       },
       data: function() {
         // Brutally remove all align classes and set a new one if "align" widget data is set.
-        this.element.removeClass( 'align--center' );
-        this.element.removeClass( 'align--left' );
-        this.element.removeClass( 'align--right' );
-        if ( this.data.align ) this.element.addClass( 'align--' + this.data.align );
+        const align = ['center', 'left', 'right',
+          'half-image-right', 'half-image-left',
+          'half-image-right-full', 'half-image-left-full',
+          'full-bleed-image'];
 
-        this.element.removeClass( 'size--full' );
-        this.element.removeClass( 'size--half' );
-        this.element.removeClass( 'size--twofifth' );
-        this.element.removeClass( 'size--third' );
-        if ( this.data.size ) this.element.addClass( 'size--' + this.data.size );
+        align.forEach(function (item, index) {
+          console.log(item, index);
+          this.element.removeClass( item );
+        });
 
+        const size = ['full_bleed_half__image', 'full_bleed__image',
+          'half__image', 'quarter',  'w',
+          'wysiwyg_callout_square_150_x_150_'];
+
+        size.forEach(function (item, index) {
+          console.log(item, index);
+          this.element.removeClass( item );
+        });
+
+        // Add new classes
+        if ( this.data.align ) this.element.addClass( this.data.align );
+        if ( this.data.size ) this.element.addClass(  this.data.size );
         if ( this.data.script ) this.element.setHtml(this.data.script)
       }
     });
