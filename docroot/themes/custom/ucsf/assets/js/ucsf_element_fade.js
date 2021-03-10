68f1d5fb264d53a5489511e499dda07d9b097e79
Eric Guerin <eric.guerin@ucsf.edu>
2021-03-09 19:34:47 -0800

    Fix again

--- docroot/themes/custom/ucsf/assets/js/ucsf_element_fade.js
+++ docroot/themes/custom/ucsf/assets/js/ucsf_element_fade.js
@@ -4,9 +4,18 @@
  * Animation of elements based on location in the viewport.
  */
 (function () {
+
+  // Elements that should not be animated with a fade
+  var excludeElements = ['.explorer .element-fade', 'dom-twocolumn-layout .element-fade', 'dom-threecolumn-layout .element-fade', '.column1 .element-fade', '.column2 .element-fade', '.column3 .element-fade', '.field-taxonomy-banner-image .element-fade', '.paragraph--type--gallery  :not(.slick-active) .element-fade'];
+
+  // Get the NodeList of all the selectors matching elements on the page.
+  var excludeSelectors = document.querySelectorAll(excludeElements.toString());
+  excludeSelectors.forEach(function (element) {
+    return element.classList.remove('element-fade');
+  });
+
   // element to be animated
   var elementFade = document.querySelectorAll('.element-fade');
-
   if ('IntersectionObserver' in window) {
     var observer = new IntersectionObserver(function (entries) {
       entries.forEach(function (entry) {
---
+++ docroot/themes/custom/ucsf/js/ucsf_element_fade.js
@@ -0,0 +1,40 @@
+/**
+ * Animation of elements based on location in the viewport.
+ */
+(function () {
+
+  // Elements that should not be animated with a fade
+  const excludeElements = [
+    '.explorer .element-fade',
+    'dom-twocolumn-layout .element-fade',
+    'dom-threecolumn-layout .element-fade',
+    '.column1 .element-fade',
+    '.column2 .element-fade',
+    '.column3 .element-fade',
+    '.field-taxonomy-banner-image .element-fade',
+    '.paragraph--type--gallery  :not(.slick-active) .element-fade'
+  ];
+
+  // Get the NodeList of all the selectors matching elements on the page.
+  const excludeSelectors = document.querySelectorAll(excludeElements.toString());
+  excludeSelectors.forEach(element => element.classList.remove('element-fade'));
+
+  // element to be animated
+  const elementFade = document.querySelectorAll('.element-fade');
+  if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          // adds the class responsible for animation on the first viewport appearance
+          entry.target.classList.add('element-fade--in-viewport');
+          observer.unobserve(entry.target);
+        }
+      })
+    })
+
+    elementFade.forEach(element => {
+      observer.observe(element);
+    });
+  }
+
+})();
