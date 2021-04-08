5841a869e32b26bbe225d939885ef5fc9f5d5d29
Sasha Miller <sashazm85@gmail.com>
2021-04-07 17:01:07 -0700

    added match media to check for mobile vs. desktop

--- docroot/themes/custom/ucsf/js/ucsf_element_swoosh.js
+++ docroot/themes/custom/ucsf/js/ucsf_element_swoosh.js
@@ -3,25 +3,33 @@
 */
 
 (function () {
-  Drupal.behaviors.elementSwoosh = {
-    attach: function (context, settings) {
-      if ('IntersectionObserver' in window) {
-        // element to be animated
-        const elementSwoosh = document.querySelector('.element-swoosh');
+  const  swooshDetect = () => {
+    Drupal.behaviors.elementSwoosh = {
+      attach: function (context, settings) {
+        if ('IntersectionObserver' in window) {
+          // element to be animated
+          const elementSwoosh = document.querySelector('.element-swoosh');
 
-        const observer = new IntersectionObserver((entry, observer) => {
-          const targetElement = entry[0].target;
-          if (entry[0].intersectionRatio > 0) {
-            // adds the class responsible for animation on the first viewport appearance
-            targetElement.classList.add('element-swoosh--in-viewport');
-            observer.unobserve(targetElement);
-          }
-        })
+          const observer = new IntersectionObserver((entry, observer) => {
+            const targetElement = entry[0].target;
+            if (entry[0].intersectionRatio > 0) {
+              // adds the class responsible for animation on the first viewport appearance
+              targetElement.classList.add('element-swoosh--in-viewport');
+              observer.unobserve(targetElement);
+            }
+          })
 
-        observer.observe(elementSwoosh);
+          observer.observe(elementSwoosh);
+        }
+
       }
-
     }
-
   };
+
+  // Use MatchMedia to ensure that events are only happening in Tablet/Desktop
+  const mql = matchMedia('(min-width: 769px)');
+  // On page load, check if desktop and listen to scroll event.
+  if (mql.matches) {
+    swooshDetect();
+  }
 })();
