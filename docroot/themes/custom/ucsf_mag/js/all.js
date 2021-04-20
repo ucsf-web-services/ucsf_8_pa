d7f75a3d87f1d78ab0d838cf3851d44888ec79d6
Sasha Miller <sashazm85@gmail.com>
2021-04-20 09:17:52 -0700

    menu header items and Javascript

--- docroot/themes/custom/ucsf_mag/js/all.js
+++ docroot/themes/custom/ucsf_mag/js/all.js
@@ -3,74 +3,62 @@
  */
  (($ => {
   // Wait for the document to be ready.
-  // $(() => {
-  //   /**
-  //   * Set aria-expanded attribute value on element that triggers the submenu visibility
-  //   * @param {jQuery Object} trigger
-  //   * @param {String} value
-  //   */
-  //   const setAria = ( trigger, value ) => {
-  //     trigger.attr('aria-expanded', value);
-  //   }
+  $(() => {
+    /**
+    * Set aria-expanded attribute value on element that triggers the submenu visibility
+    * @param {jQuery Object} trigger
+    * @param {String} value
+    */
+    const setAria = ( trigger, value ) => {
+      trigger.attr('aria-expanded', value);
+    }
 
-  //   const $navbar = $('.mag-nav');
-  //   const menuId =  document.querySelector('.mag-menu').getAttribute('id');
-  //   const $navToggle = $('.nav-toggle');
-  //   $navToggle.attr("aria-controls", menuId);
-  //   const $subnavToggle = $('.subnav-toggle');
+    const $navbar = $('.mag-nav');
+    const menuId =  document.querySelector('.mag-menu').getAttribute('id');
+    const $navToggle = $('.nav-toggle');
+    $navToggle.attr("aria-controls", menuId);
+    const $subnavToggle = $('.subnav-toggle');
 
-  //   $navToggle.on('click', function (e) {
-  //     const $this = $(this);
-  //     $this.toggleClass('nav-toggle--active');
-  //     $navbar.toggleClass('mag-nav--active');
-  //     if ($this.attr("aria-expanded") === "false") {
-  //       setAria($this, "true");
-  //     } else {
-  //       setAria($this, "false");
-  //     }
-  //   });
+    $navToggle.on('click', function (e) {
+      const $this = $(this);
+      $this.toggleClass('nav-toggle--active');
+      $navbar.toggleClass('mag-nav--active');
+      if ($this.attr("aria-expanded") === "false") {
+        setAria($this, "true");
+      } else {
+        setAria($this, "false");
+      }
+    });
 
-  //   $subnavToggle.on('click', function (e) {
-  //     const $this = $(this);
-  //     const $subnavMenuCollapsable = $this.closest('.mag-menu__item');
-  //     $subnavMenuCollapsable.toggleClass('mag-menu__item--active');
-  //     if ($this.attr("aria-expanded") === "false") {
-  //       setAria($this, "true");
-  //     } else {
-  //       setAria($this, "false");
-  //     }
-  //   });
+    // Only execute subnav extend / collapse code in mobile
+    const mobileDetect = (event) => {
+      // Mobile
+      if (event.matches) {
+      $subnavToggle.on('click', function (e) {
+        const $this = $(this);
+        const $subnavMenuCollapsable = $this.closest('.mag-menu__item');
+        $subnavMenuCollapsable.toggleClass('mag-menu__item--active');
+        if ($this.attr("aria-expanded") === "false") {
+          setAria($this, "true");
+        } else {
+          setAria($this, "false");
+        }
+      });
+    // Desktop
+    } else {
+      $subnavToggle.off('click');
+      // Remove unnecessary Aria controls from desktop subnav
+      $subnavToggle.removeAttr('aria-expanded aria-controls');
+    }
+  }
 
-  //   /**
-  //    * Run code if medium/desktop display has been detected.
-  //    *
-  //    * @param {MediaQueryList} mql
-  //    */
-  //    const desktopDetect = (mql) => {
-  //     // Desktop
-  //     if (mql.matches) {
-  //       magazineDesktopMenu();
-  //       getMenuPanelHeight();
-  //     } else {
-  //       return;
-  //     }
-  //   }
-
-  //   /**
-  //    * Watch for when the screen resizes horizontally from mobile to desktop.
-  //    */
-  //   const watchResize = () => {
-  //     // Use MatchMedia to ensure that desktop related code runs only on desktop
-  //     const mql = matchMedia('(min-width: 850px)');
-  //     // Detect Desktop on page load.
-  //     desktopDetect(mql);
-  //     // Watch to see if the page size changes.
-  //     mql.addListener(desktopDetect);
-  //   }
-
-  //   // Initialize.
-  //   watchResize();
-  // });
+  // Use MatchMedia to ensure that subnav expand/collapse is only happening in mobile
+  const mql = matchMedia('(max-width: 849px)');
+  // Detect mobile on page load.
+  mobileDetect(mql);
+  // Watch to see if the page size changes.
+  mql.addListener(mobileDetect);
+  });
 
 
 }))(jQuery);
