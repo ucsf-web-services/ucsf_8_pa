c54a915c160f38052f3980fee2cc920fed7f981d
Sasha Miller <sashazm85@gmail.com>
2022-03-17 17:34:54 -0700

    scrollyteller desktop styles, html, js

---
+++ docroot/themes/custom/ucsf/js/ucsf_parallax_image_with_text_overlay.js
@@ -0,0 +1,80 @@
+
+
+// scrollama documentation and tutoreal
+// https://www.npmjs.com/package/scrollama
+// https://www.youtube.com/watch?v=d7wTA9F-l8c
+
+// Scrollama event triggers
+// https://www.youtube.com/watch?v=rXHtHLiBWhY
+
+// instantiate the scrollama library.
+const scroller = scrollama();
+
+const gallery = document.querySelector('.scrolly-gallery');
+const items = document.querySelectorAll('.scrolly-gallery__item');
+const textItems = document.querySelectorAll('.scrolly-gallery__text');
+
+function setGalleryWidth() {
+  gallery.style.setProperty('--scrolly-gallery-width', `${gallery.offsetWidth}px`);
+}
+
+function removeCurrent() {
+  items.forEach((item) => item.classList.remove('scrolly-gallery__item--current'))
+}
+
+function removeAnimateOut() {
+  textItems.forEach((item) => item.classList.remove('scrolly-gallery__text--out'))
+}
+
+function stepEnter(response) {
+  gallery.dataset.scrollyGalleryDirection = response.direction
+
+  // 0 is the first stem in an item and 2 is the last
+  const position = (response.direction === 'down') ? 0 : 2;
+  if (response.index % 3 === position) {
+    removeCurrent();
+    response.element.parentElement.classList.add('scrolly-gallery__item--current')
+  }
+
+  // Ensure the first item gets the current class.
+  if (response.direction === 'down' && response.index === 1) {
+    response.element.parentElement.classList.add('scrolly-gallery__item--current')
+  }
+
+  if (response.element.classList.contains('scrolly-gallery__text')) {
+    removeAnimateOut();
+    response.element.classList.add('scrolly-gallery__text--active');
+    response.element.previousElementSibling.classList.add('grayscale');
+  }
+}
+
+function stepExit(response) {
+  if (response.element.classList.contains('scrolly-gallery__text')) {
+    response.element.classList.remove('scrolly-gallery__text--active');
+    response.element.classList.add('scrolly-gallery__text--out');
+    response.element.previousElementSibling.classList.remove('grayscale');
+  }
+
+  // Remove classes when exiting the gallery.
+  const exitFirst = response.direction === 'up' && response.index <= 1;
+  const exitLast = response.direction === 'down' && response.index >= ((items.length * 3) - 1);
+  if (exitFirst || exitLast) {
+    setTimeout(() => {
+      removeCurrent()
+      removeAnimateOut();
+    }, 500);
+  }
+}
+
+// Set the initial gallery width:
+setGalleryWidth();
+window.addEventListener('resize', setGalleryWidth);
+
+// setup the instance, pass callback functions
+scroller
+  .setup({
+    step: '.scrolly-gallery__step',
+  })
+  .onStepEnter(stepEnter)
+  .onStepExit(stepExit);
+
