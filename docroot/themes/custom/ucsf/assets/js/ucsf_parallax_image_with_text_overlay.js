9ee20daee64c53b72e4edbca57413e72dbbd99a5
Sasha Miller <sashazm85@gmail.com>
2022-03-31 09:43:21 -0700

    fix image in mobile not being a square

--- docroot/themes/custom/ucsf/assets/js/ucsf_parallax_image_with_text_overlay.js
+++ docroot/themes/custom/ucsf/assets/js/ucsf_parallax_image_with_text_overlay.js
@@ -10,13 +10,15 @@
 // instantiate the scrollama library.
 var scroller = scrollama();
 
-var gallery = document.querySelector('.scrolly-gallery');
+var gallery = document.querySelectorAll('.scrolly-gallery');
 var items = document.querySelectorAll('.scrolly-gallery__item');
 var textItems = document.querySelectorAll('.scrolly-gallery__text');
 var background = document.querySelectorAll('.scrolly-gallery__text-overlay');
-
+console.log(gallery);
 function setGalleryWidth() {
-  gallery.style.setProperty('--scrolly-gallery-width', gallery.offsetWidth + 'px');
+  gallery.forEach(function (item) {
+    return item.style.setProperty('--scrolly-gallery-width', item.offsetWidth + 'px');
+  });
 }
 
 function removeCurrent() {
@@ -32,7 +34,9 @@ function removeAnimateOut() {
 }
 
 function stepEnter(response) {
-  gallery.dataset.scrollyGalleryDirection = response.direction;
+  gallery.forEach(function (item) {
+    return item.dataset.scrollyGalleryDirection = response.direction;
+  });
 
   // 0 is the first stem in an item and 2 is the last
   var position = response.direction === 'down' ? 0 : 2;
@@ -49,7 +53,6 @@ function stepEnter(response) {
   if (response.element.classList.contains('scrolly-gallery__text')) {
     removeAnimateOut();
     response.element.classList.add('scrolly-gallery__text--active');
-    response.element.previousElementSibling.classList.add('grayscale');
   }
 }
 
@@ -57,7 +60,6 @@ function stepExit(response) {
   if (response.element.classList.contains('scrolly-gallery__text')) {
     response.element.classList.remove('scrolly-gallery__text--active');
     response.element.classList.add('scrolly-gallery__text--out');
-    response.element.previousElementSibling.classList.remove('grayscale');
   }
 
   // Remove classes when exiting the gallery.
--- docroot/themes/custom/ucsf/js/ucsf_parallax_image_with_text_overlay.js
+++ docroot/themes/custom/ucsf/js/ucsf_parallax_image_with_text_overlay.js
@@ -10,13 +10,13 @@
 // instantiate the scrollama library.
 const scroller = scrollama();
 
-const gallery = document.querySelector('.scrolly-gallery');
+const gallery = document.querySelectorAll('.scrolly-gallery');
 const items = document.querySelectorAll('.scrolly-gallery__item');
 const textItems = document.querySelectorAll('.scrolly-gallery__text');
 const background = document.querySelectorAll('.scrolly-gallery__text-overlay');
-
+console.log(gallery)
 function setGalleryWidth() {
-  gallery.style.setProperty('--scrolly-gallery-width', `${gallery.offsetWidth}px`);
+  gallery.forEach((item) => item.style.setProperty('--scrolly-gallery-width', `${item.offsetWidth}px`));
 }
 
 function removeCurrent() {
@@ -28,7 +28,7 @@ function removeAnimateOut() {
 }
 
 function stepEnter(response) {
-  gallery.dataset.scrollyGalleryDirection = response.direction
+  gallery.forEach((item) => item.dataset.scrollyGalleryDirection = response.direction);
 
   // 0 is the first stem in an item and 2 is the last
   const position = (response.direction === 'down') ? 0 : 2;
@@ -45,7 +45,6 @@ function stepEnter(response) {
   if (response.element.classList.contains('scrolly-gallery__text')) {
     removeAnimateOut();
     response.element.classList.add('scrolly-gallery__text--active');
-    response.element.previousElementSibling.classList.add('grayscale');
   }
 }
 
@@ -53,7 +52,6 @@ function stepExit(response) {
   if (response.element.classList.contains('scrolly-gallery__text')) {
     response.element.classList.remove('scrolly-gallery__text--active');
     response.element.classList.add('scrolly-gallery__text--out');
-    response.element.previousElementSibling.classList.remove('grayscale');
   }
 
   // Remove classes when exiting the gallery.
