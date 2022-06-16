c62ade14e25e3e18e46f86c948f9f9bfae369585
Sasha Miller <sashazm85@gmail.com>
2022-06-16 13:05:43 -0700

    rescoping the scrolly so that each one on the page is treated separately

--- docroot/themes/custom/ucsf/assets/js/ucsf_parallax_image_with_text_overlay.js
+++ docroot/themes/custom/ucsf/assets/js/ucsf_parallax_image_with_text_overlay.js
@@ -7,115 +7,107 @@
 // Scrollama event triggers
 // https://www.youtube.com/watch?v=rXHtHLiBWhY
 
-// instantiate the scrollama library.
-var scroller = scrollama();
+var galleries = document.querySelectorAll('.scrolly-gallery');
 
-var gallery = document.querySelectorAll('.scrolly-gallery');
-var items = document.querySelectorAll('.scrolly-gallery__item');
-var textItems = document.querySelectorAll('.scrolly-gallery__text');
-var background = document.querySelectorAll('.scrolly-gallery__text-overlay');
-var images = document.querySelectorAll('.scrolly-gallery__bg img');
+galleries.forEach(function (gallery) {
+  // instantiate the scrollama library.
+  var scroller = scrollama();
 
-function removeLazyLoading() {
-  setTimeout(function () {
-    images.forEach(function (image) {
-      image.removeAttribute('loading');
-      image.style.animation = 'none';
-    });
-  }, 600);
-}
-removeLazyLoading();
+  var items = gallery.querySelectorAll('.scrolly-gallery__item');
+  var textItems = gallery.querySelectorAll('.scrolly-gallery__text');
+  var images = gallery.querySelectorAll('.scrolly-gallery__bg img');
 
-function setGalleryWidth() {
-  gallery.forEach(function (item) {
-    return item.style.setProperty('--scrolly-gallery-width', item.offsetWidth + 'px');
-  });
-}
-
-function removeCurrent() {
-  items.forEach(function (item) {
-    return item.classList.remove('scrolly-gallery__item--current');
-  });
-}
-
-function removeAnimateOut() {
-  textItems.forEach(function (item) {
-    return item.classList.remove('scrolly-gallery__text--out');
-  });
-}
-
-function stepEnter(response) {
-  gallery.forEach(function (item) {
-    return item.dataset.scrollyGalleryDirection = response.direction;
-  });
-
-  // 0 is the first stem in an item and 2 is the last
-  var position = response.direction === 'down' ? 0 : 2;
-  if (response.index % 3 === position) {
-    removeCurrent();
-    response.element.parentElement.classList.add('scrolly-gallery__item--current');
-  }
-
-  // Ensure the first item gets the current class.
-  if (response.direction === 'down' && response.index === 1) {
-    response.element.parentElement.classList.add('scrolly-gallery__item--current');
-  }
-
-  if (response.element.classList.contains('scrolly-gallery__text')) {
-    removeAnimateOut();
-    response.element.classList.add('scrolly-gallery__text--active');
-  }
-}
-
-function stepExit(response) {
-  if (response.element.classList.contains('scrolly-gallery__text')) {
-    response.element.classList.remove('scrolly-gallery__text--active');
-    response.element.classList.add('scrolly-gallery__text--out');
-  }
-
-  // Remove classes when exiting the gallery.
-  var exitFirst = response.direction === 'up' && response.index <= 1;
-  var exitLast = response.direction === 'down' && response.index >= items.length * 3 - 1;
-  if (exitFirst || exitLast) {
+  function removeLazyLoading() {
     setTimeout(function () {
-      removeCurrent();
-      removeAnimateOut();
-    }, 500);
+      images.forEach(function (image) {
+        image.removeAttribute('loading');
+        image.style.animation = 'none';
+      });
+    }, 600);
   }
-}
+  removeLazyLoading();
 
-// Use MatchMedia to ensure that collision events are only happening in Desktop
-var mql = matchMedia('(min-width: 1050px)');
-// On page load, check if desktop and listen to scroll event.
-if (mql.matches) {
+  function setGalleryWidth() {
+    gallery.style.setProperty('--scrolly-gallery-width', gallery.offsetWidth + 'px');
+  }
 
-  // Set the initial gallery width:
-  setGalleryWidth();
-  window.addEventListener('resize', setGalleryWidth);
+  function removeCurrent() {
+    items.forEach(function (item) {
+      return item.classList.remove('scrolly-gallery__item--current');
+    });
+  }
 
-  // setup the instance, pass callback functions
-  scroller.setup({
-    step: '.scrolly-gallery__step'
-    // debug: true
-  }).onStepEnter(stepEnter).onStepExit(stepExit);
-}
+  function removeAnimateOut() {
+    textItems.forEach(function (item) {
+      return item.classList.remove('scrolly-gallery__text--out');
+    });
+  }
 
-// When screen size changes, add or remove the scroll listener.
-mql.addListener(function (event) {
-  if (event.matches) {
-    removeLazyLoading();
+  function stepEnter(response) {
+    gallery.dataset.scrollyGalleryDirection = response.direction;
+    // 0 is the first stem in an item and 2 is the last
+    var position = response.direction === 'down' ? 0 : 2;
+    if (response.index % 3 === position) {
+      removeCurrent();
+      response.element.parentElement.classList.add('scrolly-gallery__item--current');
+    }
 
+    // Ensure the first item gets the current class.
+    if (response.direction === 'down' && response.index === 1) {
+      response.element.parentElement.classList.add('scrolly-gallery__item--current');
+    }
+
+    if (response.element.classList.contains('scrolly-gallery__text')) {
+      removeAnimateOut();
+      response.element.classList.add('scrolly-gallery__text--active');
+    }
+  }
+
+  function stepExit(response) {
+    if (response.element.classList.contains('scrolly-gallery__text')) {
+      response.element.classList.remove('scrolly-gallery__text--active');
+      response.element.classList.add('scrolly-gallery__text--out');
+    }
+
+    // Remove classes when exiting the gallery.
+    var exitFirst = response.direction === 'up' && response.index <= 1;
+    var exitLast = response.direction === 'down' && response.index >= items.length * 3 - 1;
+    if (exitFirst || exitLast) {
+      setTimeout(function () {
+        removeCurrent();
+        removeAnimateOut();
+      }, 500);
+    }
+  }
+
+  function init() {
     // Set the initial gallery width:
     setGalleryWidth();
     window.addEventListener('resize', setGalleryWidth);
 
     // setup the instance, pass callback functions
     scroller.setup({
-      step: '.scrolly-gallery__step'
+      step: gallery.querySelectorAll('.scrolly-gallery__step')
+      // debug: true
     }).onStepEnter(stepEnter).onStepExit(stepExit);
-  } else {
-    window.removeEventListener('resize', setGalleryWidth);
-    scroller.destroy();
   }
+
+  // Use MatchMedia to ensure that collision events are only happening in Desktop
+  var mql = matchMedia('(min-width: 1050px)');
+  // On page load, check if desktop and listen to scroll event.
+  if (mql.matches) {
+    removeLazyLoading();
+    init();
+  }
+
+  // When screen size changes, add or remove the scroll listener.
+  mql.addListener(function (event) {
+    if (event.matches) {
+      init();
+    } else {
+      window.removeEventListener('resize', setGalleryWidth);
+      scroller.destroy();
+    }
+  });
 });
 //# sourceMappingURL=ucsf_parallax_image_with_text_overlay.js.map
